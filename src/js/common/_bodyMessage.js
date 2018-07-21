04d043e78f539f257024797e3e0c72d9d0cd608c
Daiki Saito <dsflon@gmail.com>
2018-07-21 12:49:40 +0900

    メッセージバグ修正

--- src/js/common/_bodyMessage.js
+++ src/js/common/_bodyMessage.js
@@ -27,13 +27,14 @@ class BodyMessage {
 
     Remove() {
 
-        const TransitionEnd = () => {
-            this.target.dataset.message = "";
-            this.target.removeEventListener("transitionend", TransitionEnd.bind(this))
+        const TransitionEnd = (e) => {
+            let target = e.currentTarget;
+            target.dataset.message = "";
+            target.removeEventListener("transitionend", TransitionEnd)
         }
 
         this.target.classList.remove("show_message");
-        this.target.addEventListener("transitionend", TransitionEnd.bind(this))
+        this.target.addEventListener("transitionend", TransitionEnd)
 
     }
 
